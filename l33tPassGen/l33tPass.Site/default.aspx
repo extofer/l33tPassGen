9b8f2c7f9b1fbfc45ee279082c8b1471c1dafcfb
Gabriel <extofer@gmail.com>
2012-07-08 00:50:29 -0600

    Added random read from word list and added boostrap css/js

--- l33tPassGen/l33tPass.Site/default.aspx
+++ l33tPassGen/l33tPass.Site/default.aspx
@@ -4,9 +4,47 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>l33t p@ssword generat0r</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<style>
+	body {
+	  padding-top: 60px;
+	  padding-bottom: 40px;
+	}
+	</style>
+	<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/libs/modernizr-2.5.3-respond-1.1.0.min.js" type="text/javascript"></script>
 </head>
 <body>
+        <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">l33t p@ssword generat0r</a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
     <form id="form1" runat="server">
     <div>
 
@@ -23,5 +61,6 @@
            
 
     </form>
+   </div>
 </body>
 </html>
--- l33tPassGen/l33tPass.Site/default.aspx.cs
+++ l33tPassGen/l33tPass.Site/default.aspx.cs
@@ -4,6 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.IO;
+
+
 using l33tPassGen;
 
 namespace l33tPass.Site
@@ -22,12 +41,41 @@ namespace l33tPass.Site
 
             if (textBox1.Text == "")
             {
-                richTextBox1.Value = oleet.ReplaceWord("CONVENTIONAL");
+                richTextBox1.Value = oleet.ReplaceWord(ReadTextFile());
             }
             else
             {
                 richTextBox1.Value = oleet.ReplaceWord(textBox1.Text.ToString());
             }
+        }
+
+        
+        string ReadTextFile()
+        {
+
+            using (StreamReader streamReader = File.OpenText(Server.MapPath("~") + @"\12letterwords.txt"))
+            {
+                    int count = 0;
+                    List<string> wordlist = new List<string>();
+
+                    string inputString;
+
+                    while ((inputString = streamReader.ReadLine()) != null)
+                    {
+                        wordlist.Add(inputString);
+                        inputString = streamReader.ReadLine();
+                        count++;
+                    }
+
+                    Random random = new Random();
+                    int rand = random.Next(count);
+
+
+                    return wordlist[rand];
+            }
+
+            
+
         }
 
 
